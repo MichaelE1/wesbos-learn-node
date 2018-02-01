6b31908005580de7f4d8774ac6a82f0999755aee
Michael <mickel13@gmail.com>
2018-02-01 17:51:25 +1000

    completed lessons 12-14

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -10,7 +10,35 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // Query the database for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', {title: 'Stores', stores});
+};
+
+exports.editStore = async (req, res) => {
+  // Find the store given the ID
+  const store = await Store.findOne({_id: req.params.id});
+  // Confirm they are the owner of the store
+  // TO DO
+  // Render out the dit form so the user can update their store
+  res.render('editStore', {title: `Edit ${store.name}`, store});
+};
+
+exports.updateStore = async (req, res) => {
+  // Find an update the store
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store --></a>`);
+  
+  // Redirect them to the store and tell them it worked
+  res.redirect(`/stores/${store._id}/edit`);
 };
