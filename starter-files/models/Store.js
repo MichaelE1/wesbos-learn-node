c2827954abb00f5506f7410bfd25146b350f94de
Michael <mickel13@gmail.com>
2018-02-09 19:23:48 +1000

    completed lessons 19 & 20

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -35,15 +35,20 @@ const storeSchema = new mongoose.Schema({
   photo: String,
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stop this function from running
   }
   this.slug = slug(this.name);
-  next();
+  // find other stores that have a slug of wes, wes-1, wes-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({slug: slugRegEx});
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
 
-  // TO DO: make more resilient so slugs are more unique
+  next();
 });
 
 module.exports = mongoose.model('Store', storeSchema);
