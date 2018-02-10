02a35d7136c90f97548c23ad0212fab5d6696c49
Michael <mickel13@gmail.com>
2018-02-10 12:34:04 +1000

    completed lessons 21 & 22

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -83,4 +83,13 @@ exports.getStoreBySlug = async (req, res, next) => {
   const store = await Store.findOne({slug: req.params.slug});
   if (!store) return next();
   res.render('store', {store, title: store.name});
-}
+};
+
+exports.getStoresByTag = async (req, res) => {
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true};
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags : tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);  
+  res.render('tag', { tags, title: 'Tags', tag, stores });
+};
