93ce4d516db0137407d847f6409225e105b3108c
Michael <mickel13@gmail.com>
2018-02-24 18:14:08 +1000

    began lesson 32

---
+++ starter-files/public/javascripts/modules/typeAhead.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+function searchResultsHTML(stores) {
+  return stores.map(store => {
+    return `
+      <a href="/stores/${store.slug}" class="search__result">
+      <strong>${store.name}</strong>
+      </a>
+      `;
+  }).join('');
+};
+
+function typeAhead(search) {
+  if (!search) return;
+
+  const searchInput = search.querySelector('input[name="search"');
+  const searchResults = search.querySelector('.search__results');
+
+  searchInput.on('input', function() {
+    // if there is no value, quit it!
+    if (!this.value) {
+      searchResults.style.display = 'none';
+      return; // stop
+    }
+
+    // show the search results
+    searchResults.style.display = 'block';
+
+    axios
+      .get(`/api/search?q=${this.value}`)
+      .then(res => {
+        if (res.data.length) {
+          searchResults.innerHTML = searchResultsHTML(res.data);
+        }
+      })
+  });
+}
+
+export default typeAhead;
