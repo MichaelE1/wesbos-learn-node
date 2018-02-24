3be2b1f09fbb1cf68a5c19f47a3da92734a90ccd
Michael <mickel13@gmail.com>
2018-02-24 21:06:12 +1000

    completed lesson 32

--- starter-files/public/javascripts/modules/typeAhead.js
+++ starter-files/public/javascripts/modules/typeAhead.js
@@ -1,9 +1,10 @@
-const axios = require('axios');
+import axios from 'axios';
+import dompurify from 'dompurify';
 
 function searchResultsHTML(stores) {
   return stores.map(store => {
     return `
-      <a href="/stores/${store.slug}" class="search__result">
+      <a href="/store/${store.slug}" class="search__result">
       <strong>${store.name}</strong>
       </a>
       `;
@@ -30,9 +31,44 @@ function typeAhead(search) {
       .get(`/api/search?q=${this.value}`)
       .then(res => {
         if (res.data.length) {
-          searchResults.innerHTML = searchResultsHTML(res.data);
+          searchResults.innerHTML = dompurify.sanitize(searchResultsHTML(res.data));
+          return;
         }
+        // tell them nothing came back
+        searchResults.innerHTML = dompurify.sanitize(`<div class="search__result">No results for ${this.value} found!</div>`);
       })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+
+  // handle keyboard inputs
+  searchInput.on('keyup', (e) => {
+    // if not pressing up, down, or enter, who cares
+    if (![38, 40, 13].includes(e.keyCode)) {
+      return;
+    }
+    const activeClass = 'search__result--active';
+    const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll('.search__result');
+    let next;
+    if (e.keyCode === 40 && current) {
+      next = current.nextElementSibling || items[0];
+    } else if (e.keyCode === 40) {
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keyCode === 38) {
+      next = items[items.length - 1];
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
+      return;
+    }
+
+    if (current) {
+      current.classList.remove(activeClass);
+    }
+    next.classList.add(activeClass); 
   });
 }
 
